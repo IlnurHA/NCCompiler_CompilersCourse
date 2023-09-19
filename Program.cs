3a39a28d351ca03610cee57c4bd556cc5889a54b
zaqbez39me <korolov_k@mail.ru>
2023-09-19 20:29:36 +0300

    Changed tests choosing via passing absolute path in console instead of reading from specific directory

--- Program.cs
+++ Program.cs
@@ -4,18 +4,15 @@ class Program
 {
     public static void Main(string[] args)
     {
-        string fullPathToTests =
-            Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "tests");
-        var allTests = Directory.EnumerateFiles(fullPathToTests, "*.ncc");
-        foreach (var filename in allTests)
+        
+        string? fileName;
+        while ((fileName = Console.ReadLine()) == null) { }
+        string contents = File.ReadAllText(fileName);
+        Lexer lexer = new Lexer(contents);
+        List<Token> tokens = lexer.Tokenize();
+        foreach (Token token in tokens)
         {
-            string contents = File.ReadAllText(filename);
-            Lexer lexer = new Lexer(contents);
-            List<Token> tokens = lexer.Tokenize();
-            foreach (Token token in tokens)
-            {
-                Console.WriteLine($"Type: {token.Type}, Lexeme: \"{token.Lexeme}\", Value: {token.Value}");
-            }
+            Console.WriteLine($"Type: {token.Type}, Lexeme: \"{token.Lexeme}\", Value: {token.Value}");
         }
     }
 }
