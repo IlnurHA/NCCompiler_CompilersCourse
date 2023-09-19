e60992197e4c7ed4ba6ddd043fe7e2e176d18903
kamil <almetov.kamil@gmail.com>
2023-09-19 20:53:33 +0300

    Add regexps

--- Lexer.cs
+++ Lexer.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace NCCompiler_CompilersCourse;
 
 public class Lexer
@@ -24,16 +26,60 @@ public class Lexer
             ")" => TokenType.RightBracket,
             "[" => TokenType.LeftSquaredBracket,
             "]" => TokenType.RightSquaredBracket,
-            "=" => TokenType.EqComparison,
             "," => TokenType.Comma,
             "\n" => TokenType.EndOfLine,
+            ":" => TokenType.Colon,
+            "." => TokenType.Dot,
+            ".." => TokenType.TwoDots,
+            "and" => TokenType.And,
+            "or" => TokenType.Or,
+            "xor" => TokenType.Xor,
+            "=" => TokenType.EqComparison,
+            ">" => TokenType.GtComparison,
+            ">=" => TokenType.GeComparison,
+            "<" => TokenType.LtComparisom,
+            "<=" => TokenType.LeComparison,
+            "<>" => TokenType.NeComparison,
+            "//" => TokenType.SinglelineComment,
+            "/*" => TokenType.MultilineCommentStart,
+            "*/" => TokenType.MultilineCommentEnd,
+            ":=" => TokenType.AssignmentOperator,
+            "routine" => TokenType.Routine,
+            "array" => TokenType.Array,
+            "integer" => TokenType.Integer,
+            "is" => TokenType.Is,
+            "var" => TokenType.Var,
+            "size" => TokenType.Size,
+            "for" => TokenType.For,
+            "from" => TokenType.From,
+            "loop" => TokenType.Loop,
+            "if" => TokenType.If,
+            "then" => TokenType.Then,
+            "else" => TokenType.Else,
+            "end" => TokenType.End,
+            "return" => TokenType.Return,
+            "real" => TokenType.Real,
+            "in" => TokenType.In,
+            "assert" => TokenType.Assert,
+            "while" => TokenType.While,
+            "type" => TokenType.Type,
+            "record" => TokenType.Record,
+            "true" => TokenType.True,
+            "false" => TokenType.False,
+            "boolean" => TokenType.Boolean,
+            var someVal when new Regex(@"^\w[\w\d_-]*$").IsMatch(someVal) =>
+                TokenType.Identifier,
+            var someVal when new Regex(@"^-?\d+$").IsMatch(someVal) =>
+                TokenType.Number,
+            var someVal when new Regex(@"^-?\d+\.?\d+$").IsMatch(someVal) =>
+                TokenType.Float,
             _ => throw new NotImplementedException("The token is not implemented")
         };
     }
 
     public List<Token> Tokenize()
     {
-        int lineCounter = 1;
+        var lineCounter = 1;
         var lastEolIndex = 0;
         while (_currentPosition < _input.Length)
         {
@@ -63,46 +109,48 @@ public class Lexer
                 while (
                     lexemeLength + _currentPosition < _input.Length &&
                     (char.IsLetterOrDigit(_input[lexemeLength + _currentPosition]) ||
-                    _input[lexemeLength + _currentPosition] == '_')
+                     _input[lexemeLength + _currentPosition] == '_')
                 ) lexemeLength++;
             }
             else
             {
-                if (_currentPosition + 1 < input.Length &&
-                    ((currentChar == '/' && input[_currentPosition + 1] == '=') ||
-                     (currentChar == '/' && input[_currentPosition + 1] == '/') ||
-                     (currentChar == '/' && input[_currentPosition + 1] == '*') ||
-                     (currentChar == '*' && input[_currentPosition + 1] == '/') ||
-                     (currentChar == ':' && input[_currentPosition + 1] == '=') ||
-                     (currentChar == '>' && input[_currentPosition + 1] == '=') ||
-                     (currentChar == '<' && input[_currentPosition + 1] == '=') ||
-                     (currentChar == '.' && input[_currentPosition + 1] == '.')))
+                if (_currentPosition + 1 < _input.Length &&
+                    ((currentChar == '/' && _input[_currentPosition + 1] == '=') ||
+                     (currentChar == '/' && _input[_currentPosition + 1] == '/') ||
+                     (currentChar == '/' && _input[_currentPosition + 1] == '*') ||
+                     (currentChar == '*' && _input[_currentPosition + 1] == '/') ||
+                     (currentChar == ':' && _input[_currentPosition + 1] == '=') ||
+                     (currentChar == '>' && _input[_currentPosition + 1] == '=') ||
+                     (currentChar == '<' && _input[_currentPosition + 1] == '=') ||
+                     (currentChar == '.' && _input[_currentPosition + 1] == '.')))
                 {
-                    lexemeLength = 2
+                    lexemeLength = 2;
                 }
                 else
                 {
-                    lexemeLength = 1
+                    lexemeLength = 1;
                 }
             }
 
             var substring = _input.Substring(_currentPosition, lexemeLength);
-                _tokens.Add(
-                    new Token(
-                        type: GetTokenType(substring),
-                        lexeme: substring,
-                        span: new Span(
-                            lineNum: lineCounter,
-                            posBegin: _currentPosition - lastEolIndex,
-                            posEnd: _currentPosition + lexemeLength - lastEolIndex
-                        )
+            _tokens.Add(
+                new Token(
+                    type: GetTokenType(substring),
+                    lexeme: substring,
+                    span: new Span(
+                        lineNum: lineCounter,
+                        posBegin: _currentPosition - lastEolIndex,
+                        posEnd: _currentPosition + lexemeLength - lastEolIndex
                     )
-                );
+                )
+            );
             if (currentChar == '\n')
             {
                 lineCounter++;
-                lastEolIndex = currentPosition;
+                lastEolIndex = _currentPosition;
             }
+
+            _currentPosition++;
         }
 
         // tokens.Add(new Token(TokenType.EOF, "")); // End of file marker
