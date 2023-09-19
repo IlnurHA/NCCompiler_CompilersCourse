32d0bdccd7950affcdf6c4248917adda41f98db0
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2023-09-19 16:22:33 +0300

    Tokens added

--- TokenType.cs
+++ TokenType.cs
@@ -3,12 +3,44 @@ namespace NCCompiler_CompilersCourse;
 public enum TokenType
 {
     // Tokens without values
-    Plus, Minus, Multiply, Divide,
-    LeftParen, RightParen,
-    
+    routine, array, integer, is,
+    var, size, for, from, loop,
+    if, then, else, end, return, real,
+    in, assert, while, and, or, xor, type,
+    record, true, false, boolean,
+
     // Tokens with values
-    Identifier, Number,
+    identifier, number, float,
     
     // Other tokens
-    EOF
+    end_of_line,
+    colon, dot, two_dots,
+    comma,
+    
+    // Comments
+    multiline_comment_start,
+    multiline_comment_end,
+    singleline_comment,
+    
+    assignment_operator,
+    
+    // Boolean operators
+    eq_comparison,
+    gr_comparison,
+    greq_comparison,
+    l_comparisom,
+    leq_comparison,
+    neq_comparison,
+    
+    // Arithmetic operators
+    plus,
+    minus,
+    division,
+    remainder,
+    
+    // Brackets
+    left_bracket,
+    right_bracket,
+    left_squared_bracket,
+    right_squared_bracket,
 }
