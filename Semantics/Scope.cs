b41cf1ec3d604b76f56afbc5e6d96db4a30a66cd
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2023-11-11 13:29:08 +0300

    Scope now is counting references to variables

--- Semantics/Scope.cs
+++ Semantics/Scope.cs
@@ -9,43 +9,78 @@ public class Scope
         Routine,
         IfStatement
     }
-    
-    public Dictionary<string, SymbolicNode> Variables { get; set; } = new();
-    public ScopeContext ScopeContextVar { get; }
-    public string? ScopeContextName { get; } 
-    
-    public Scope(Dictionary<string, SymbolicNode>? variables = null, ScopeContext scopeContext = ScopeContext.Global, string? scopeContextName = null)
-    {
-        if (variables != null)
-        {
-            Variables = variables;
-        }
 
+    public Dictionary<string, (VarNode, int)> Variables { get; set; } = new();
+    public ScopeContext ScopeContextVar { get; }
+    public string? ScopeContextName { get; }
+
+    public Scope(ScopeContext scopeContext = ScopeContext.Global, string? scopeContextName = null)
+    {
         ScopeContextVar = scopeContext;
         ScopeContextName = scopeContextName;
     }
 
-    public SymbolicNode? FindVariable(string name)
+    public bool IsFree(string name)
     {
-        return Variables.TryGetValue(name, out var variable) ? variable : null;
+        return Variables.TryGetValue(name, out _);
     }
 
-    public void AddVariable(SymbolicNode node)
+    public VarNode? FindVariable(string name)
+    {
+        var isFound = Variables.TryGetValue(name, out var result);
+        if (isFound)
+        {
+            Variables[name] = (result.Item1, result.Item2 + 1);
+            return result.Item1;
+        }
+
+        return null;
+    }
+
+    public void AddVariable(VarNode node)
     {
         if (node.Name == null)
         {
             throw new Exception("Trying to save not variable as a variable");
         }
 
-        if (Variables.TryGetValue(node.Name, out var variable))
+        if (Variables.TryGetValue(node.Name, out var result))
         {
-            if (variable.MyType != node.MyType || (variable.CompoundType != null && !variable.CompoundType.Equals(node.CompoundType)))
+            var (variable, _) = result;
+            if (!CheckTypes(variable.Type, node.Type))
             {
-                throw new Exception("Cannot define variable second time with different type");
+                throw new Exception($"Cannot define variable second time with different type: {node.Name} - type1 {variable.Type}, type2 {node.Type}");
             }
 
-            Variables[node.Name] = node;
+            Variables[node.Name] = (node, 0);
         }
-        else Variables.Add(node.Name, node);
+        else Variables.Add(node.Name, (node, 0));
+    }
+
+    public void UpdateValue(string name, ValueNode value)
+    {
+        var variable = FindVariable(name);
+        if (variable is null) throw new Exception("No variable with this name");
+        if (!CheckTypes(variable.Type, value.Type))
+            throw new Exception(
+                $"Trying to update with different types: {name} - type1 {variable.Value!.GetType()}, type2 {value.GetType()}");
+
+        variable.Value = value.Value;
+    }
+
+    public List<VarNode> GetUnusedVariables()
+    {
+        List<VarNode> unusedVariables = new List<VarNode>(Variables.Count);
+        foreach (var variable in Variables)
+        {
+            if (variable.Value.Item2 == 0) unusedVariables.Add(variable.Value.Item1);
+        }
+
+        return unusedVariables;
+    }
+
+    private bool CheckTypes(TypeNode valueA, TypeNode valueB)
+    {
+        return valueA == valueB;
     }
 }
