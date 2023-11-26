18c2285893d593c8db75e4de911816c2af762b10
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2023-11-26 18:43:43 +0300

    CodeGen

---
+++ CodeGeneration/Command.cs
@@ -0,0 +1,98 @@
+﻿namespace NCCompiler_CompilersCourse.CodeGeneration;
+
+public abstract class BaseCommand
+{
+    public abstract string Translate();
+}
+
+public class CallCommand : BaseCommand
+{
+    public string FunctionName { get; set; }
+
+    public CallCommand(string functionName)
+    {
+        FunctionName = functionName;
+    }
+
+    public override string Translate()
+    {
+        return "call" + '\t' + FunctionName;
+    }
+}
+
+public class JumpCommand : BaseCommand
+{
+    public string Address { get; set; }
+
+    public void SetAddress(string address)
+    {
+        Address = address;
+    }
+
+    public override string Translate()
+    {
+        return "br.s" + '\t' + Address;
+    }
+}
+
+public class JumpForBreakCommand : JumpCommand
+{
+}
+
+public class JumpIfTrue : JumpCommand
+{
+    public override string Translate()
+    {
+        return "brtrue.s" + '\t' + Address;
+    }
+}
+
+public abstract class LocalVarCommand : BaseCommand
+{
+    public int Index { get; set; }
+
+    public LocalVarCommand(int index)
+    {
+        Index = index;
+    }
+}
+
+public class LoadLocalCommand : LocalVarCommand
+{
+    public LoadLocalCommand(int index) : base(index)
+    {
+    }
+
+    public override string Translate()
+    {
+        return $"ldloc.{Index}";
+    }
+}
+
+public class SetLocalCommand : LocalVarCommand
+{
+    public SetLocalCommand(int index) : base(index)
+    {
+    }
+
+    public override string Translate()
+    {
+        return $"stloc.{Index}";
+    }
+}
+
+public class ReturnCommand : BaseCommand
+{
+    public override string Translate()
+    {
+        return "ret";
+    }
+}
+
+public class DuplicateCommand : BaseCommand
+{
+    public override string Translate()
+    {
+        return "dup";
+    }
+}
--- CodeGeneration/TranslationVisitor.cs
+++ CodeGeneration/TranslationVisitor.cs
@@ -2,10 +2,165 @@
 
 namespace NCCompiler_CompilersCourse.CodeGeneration;
 
-// public class TranslationVisitor : IVisitor
-// {
-//     public void VisitValueVariableDeclaration(ValueVariableDeclaration valueVariableDeclaration, Queue<string> commands)
-//     {
-//         throw new NotImplementedException();
-//     }
-// }
+public class TranslationVisitor : IVisitor
+{
+    public void VisitProgramNode(ProgramNode programNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitGetFieldNode(GetFieldNode getFieldNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitGetByIndexNode(GetByIndexNode getByIndexNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitSortedArrayNode(SortedArrayNode sortedArrayNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitArraySizeNode(ArraySizeNode arraySizeNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitReversedArrayNode(ReversedArrayNode reversedArrayNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitTypeVariableDeclaration(TypeVariableDeclaration typeVariableDeclaration, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitValueVariableDeclaration(ValueVariableDeclaration valueVariableDeclaration, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitFullVariableDeclaration(FullVariableDeclaration fullVariableDeclaration, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitVariableDeclarations(VariableDeclarations variableDeclarations, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitBreakNode(BreakNode breakNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitAssertNode(AssertNode assertNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitValueReturnNode(ValueReturnNode valueReturnNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitRangeNode(RangeNode rangeNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitForLoopNode(ForLoopNode forLoopNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitForEachLoopNode(ForEachLoopNode forEachLoopNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitWhileLoopNode(WhileLoopNode whileLoopNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitIfStatement(IfStatement ifStatement, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitIfElseStatement(IfElseStatement ifElseStatement, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitBodyNode(BodyNode bodyNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitAssignmentNode(AssignmentNode assignmentNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitArrayTypeNode(ArrayTypeNode arrayTypeNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitStructTypeNode(StructTypeNode structTypeNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitCastNode(CastNode castNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitRoutineDeclarationNode(RoutineDeclarationNode routineDeclarationNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitParametersNode(ParametersNode parametersNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitRoutineCallNode(RoutineCallNode routineCallNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitExpressionsNode(ExpressionsNode expressionsNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitConstNode(ConstNode constNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitOperationNode(OperationNode operationNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitArrayConst(ArrayConst arrayConst, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitPrimitiveVarNode(PrimitiveVarNode primitiveVarNode, Queue<string> commands)
+    {
+        throw new NotImplementedException();
+    }
+}
